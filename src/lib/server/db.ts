854eccb1cac4da3a883b99648d5154ee43cc7bf5
jpnc1695 <joaopaulonunescosta@gmail.com>
2025-04-07 16:22:52 -0300

    atualizacao db.ts .env

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -2,9 +2,9 @@ import { drizzle } from "drizzle-orm/planetscale-serverless";
 import mysql from "mysql2/promise";
 
 const connection = await mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    database:"cadastrodeempresas"
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    database:process.env.DB_DATABASE
 })
 
 const db = drizzle({client:connection})
